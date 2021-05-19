bb03dc32a822afb5acee89d6c092c18cd628dc6e
milenabarot <milenabarot@gmail.com>
2021-05-19 11:53:13 +0100

    Initial use of API and declared recipeList state

--- recipefrontend/src/App.js
+++ recipefrontend/src/App.js
@@ -1,11 +1,67 @@
 import "./App.css";
+import createReactClass from "create-react-class";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
-}
+const App = createReactClass({
+  getInitialState() {
+    return {
+      searchValue: "",
+      recipeList: [],
+    };
+  },
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/posts")
+      // .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+
+        const recipes = response.data.map((recipeData) => {
+          const title = recipeData.title;
+          const description = recipeData.description;
+          const image = recipeData.image;
+          const urlLink = recipeData.url;
+          const dateAdded = recipeData.dateAdded;
+          return {
+            title: title,
+            description: description,
+            image: image,
+            url: urlLink,
+            dateAdded: dateAdded,
+          };
+        });
+        this.setState({
+          recipeList: recipes,
+        });
+        console.log(this.state.recipeList);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  //   searchOnInputKeyDown(event){
+  //     if (event.key === 'Enter') {
+
+  //    }
+  //  },
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          className="searchValue"
+          value={this.state.searchValue}
+          onInput={(event) => {
+            this.setState({ searchValue: event.target.value });
+          }}
+        />
+        <button className="searchButton">Search</button>
+      </div>
+    );
+  },
+});
 
 export default App;
