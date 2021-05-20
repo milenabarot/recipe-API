3407da3fdc9bb13b563f9cd705e5709ef1a6aa90
milenabarot <milenabarot@gmail.com>
2021-05-20 12:46:03 +0100

    Delete functionality
    - Delete functionality
    - Edited dateAdded format for when newrecipe is submitted function, so stays in same format : day/month/date/year

--- recipefrontend/src/App.js
+++ recipefrontend/src/App.js
@@ -40,12 +40,14 @@ const App = createReactClass({
           const urlLink = recipeData.url;
           const dateAdded = recipeData.dateAdded;
           const dateAddedFormatted = new Date(dateAdded).toDateString();
+          const id = recipeData._id;
           return {
             title: title,
             description: description,
             image: image,
             url: urlLink,
             dateAdded: dateAddedFormatted,
+            id: id,
           };
         });
         this.setState({
@@ -58,13 +60,27 @@ const App = createReactClass({
       });
   },
 
+  //add new recipe
   newRecipeOnSubmit(event) {
     event.preventDefault();
     axios
       .post("http://localhost:3000/posts", this.state.newRecipe)
       .then((response) => {
         let { recipeList } = this.state;
-        recipeList.push(response.data);
+
+        // assigning response.data to newRecipe, so can edit format of date
+        // and then pass newRecipeEditedDate in to be pushed onto recipeList
+        const newRecipe = response.data;
+        const recipeListDateAdded = new Date(
+          response.data.dateAdded
+        ).toDateString();
+
+        const newRecipeEditedDate = {
+          ...newRecipe,
+          dateAdded: recipeListDateAdded,
+        };
+
+        recipeList.push(newRecipeEditedDate);
         console.log(response.data);
         this.setState({
           recipeList,
@@ -76,6 +92,47 @@ const App = createReactClass({
             dateAdded: new Date(),
           },
         });
+        console.log(this.state.recipeList, this.state.newRecipe);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  //delete recipe
+  deleteRecipe(id) {
+    axios.delete("http://localhost:3000/posts/" + id).then((response) => {
+      this.refreshRecipeList();
+    });
+  },
+
+  refreshRecipeList() {
+    axios
+      .get("http://localhost:3000/posts/")
+      .then((response) => {
+        console.log(response);
+
+        const recipes = response.data.map((recipeData) => {
+          const title = recipeData.title;
+          const description = recipeData.description;
+          const image = recipeData.image;
+          const urlLink = recipeData.url;
+          const dateAdded = recipeData.dateAdded;
+          const dateAddedFormatted = new Date(dateAdded).toDateString();
+          const id = recipeData._id;
+          return {
+            title: title,
+            description: description,
+            image: image,
+            url: urlLink,
+            dateAdded: dateAddedFormatted,
+            id: id,
+          };
+        });
+        this.setState({
+          recipeList: recipes,
+        });
+        console.log(this.state.recipeList);
       })
       .catch((error) => {
         console.error(error);
@@ -101,7 +158,7 @@ const App = createReactClass({
             id="title"
             name="title"
             value={this.state.newRecipe.title}
-            placeholder="title"
+            placeholder="Recipe Title"
             required
             onChange={this.newRecipeInputChange}
           />
@@ -111,17 +168,17 @@ const App = createReactClass({
             id="description"
             name="description"
             value={this.state.newRecipe.description}
-            placeholder="description"
+            placeholder="Description"
             required
             onChange={this.newRecipeInputChange}
           />
           <label htmlFor="image"></label>
           <input
             type="text"
-            id="picture"
+            id="image"
             name="image"
             value={this.state.newRecipe.image}
-            placeholder="picture url"
+            placeholder="Image url"
             required
             onChange={this.newRecipeInputChange}
           />
@@ -131,7 +188,7 @@ const App = createReactClass({
             id="newrecipeurl"
             name="url"
             value={this.state.newRecipe.url}
-            placeholder="link url"
+            placeholder="Link url"
             required
             onChange={this.newRecipeInputChange}
           />
@@ -139,10 +196,17 @@ const App = createReactClass({
         </form>
 
         <ul className="recipeList">
-          {this.state.recipeList.map((recipe) => {
+          {this.state.recipeList.map((recipe, index, _id) => {
             return (
-              <li key={recipe.url} className="recipe">
-                <p>{recipe.title}</p>
+              <li key={index} className="recipe">
+                <label htmlFor="recipeTitle"></label>
+                <input
+                  type="text"
+                  id="recipetitle"
+                  name="recipeTitle"
+                  value={recipe.title}
+                  required
+                ></input>
                 <p>{recipe.description}</p>
                 <img
                   src={recipe.image}
@@ -153,6 +217,9 @@ const App = createReactClass({
                   Link to recipe
                 </a>
                 <p>{recipe.dateAdded}</p>
+                <button onClick={() => this.deleteRecipe(recipe.id)}>
+                  Delete
+                </button>
               </li>
             );
           })}
