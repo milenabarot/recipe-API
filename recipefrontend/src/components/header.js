76d2c82b5356bdda1f8e606337c436386dbc2cdb
milenabarot <milenabarot@gmail.com>
2021-06-21 10:56:21 +0100

    Confetti Button
    - Changed text in button depending on the state of isConfettiShowing.
    - Styling and made position fixed in top left corner.

--- recipefrontend/src/components/header.js
+++ recipefrontend/src/components/header.js
@@ -18,25 +18,27 @@ const Header = createReactClass({
   render() {
     const width = document.documentElement.scrollWidth;
     const height = document.documentElement.scrollHeight;
-    console.log(width, height);
+
     return (
       <div className="header">
-        <button onClick={this.toggleConfetti}>Click me!</button>
+        <button className="header-confettiButton" onClick={this.toggleConfetti}>
+          {this.state.isConfettiShowing ? <p>Turn me off</p> : <p>Click me!</p>}
+        </button>
         <h1>Recipe App</h1>
         {this.state.isConfettiShowing && (
           <Confetti
             width={width}
             height={height}
             colors={[
-              "#f44336",
-              "#e91e63",
-              "#9c27b0",
-              "#673ab7",
-              "#3f51b5",
-              "#2196f3",
-              "#03a9f4",
+              "#00bcd4",
+              "#009688",
+              "#4CAF50",
+              "#8BC34A",
+              "#CDDC39",
+              "#FFEB3B",
+              "#FFC107",
+              "#FF9800",
             ]}
-            recycle={false}
           />
         )}
       </div>
